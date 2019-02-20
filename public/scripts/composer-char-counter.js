a88f2557fbde6cc84897a96d1410d7041b69a05a
GideonBrasil <gidxx_z@yahoo.com>
2019-02-20 15:19:45 +0000

    target the span counter and update it with the char counter value

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
-  let textLength;
   $('.new-tweet').on('keypress', 'textarea', function() {
-    textLength = 140 - (this.textLength);
-    $('.counter') 
+    let textLength = 140 - (this.textLength);
     console.log(textLength);
+    let counter = $(this).siblings('.counter').text(textLength);
+    console.log('counter:', counter);
+    // $(counter).text 
   });
 });
