0524b0d83bd4f2278e1f3012ee87277c7456ab1e
GideonBrasil <gidxx_z@yahoo.com>
2019-02-20 15:41:42 +0000

    made the span counter red when past 140 character and go back to blak if black  <= 140

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
-  $('.new-tweet').on('keypress', 'textarea', function() {
+  $('.new-tweet').on('keydown', 'textarea', function() {
     let textLength = 140 - (this.textLength);
-    console.log(textLength);
+    // console.log(textLength);
     let counter = $(this).siblings('.counter').text(textLength);
-    console.log('counter:', counter);
-    // $(counter).text 
+    // console.log('counter:', counter);
+    if (textLength < 0) {
+      $(this).siblings('.counter').css('color', 'red');
+    } else {
+      $(this).siblings('.counter').css('color', 'black');
+    }
   });
 });
