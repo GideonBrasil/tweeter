ceca263e0ca6da1aeaa9d2a0d1024ffde2e77ef9
GideonBrasil <gidxx_z@yahoo.com>
2019-02-20 14:44:54 +0000

    declared textLength before jquery new-tweet

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
+  let textLength;
   $('.new-tweet').on('keypress', 'textarea', function() {
     textLength = 140 - (this.textLength);
+    $('.counter') 
     console.log(textLength);
   });
 });
