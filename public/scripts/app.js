892d8fa7989cd1059120c74bd676c63410fb40c3
GideonBrasil <gidxx_z@yahoo.com>
2019-02-23 00:17:50 +0000

    add mongo_examples to gitignore, clean up code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,57 +13,56 @@ $(() => {
     return div.innerHTML;
   }
 
-function createTweetElement(data) {
-	return `
-		<article class="tweet">
-			<header class="tweet-header">
-				<img class="avatar" src="${data.user.avatars.small}" />
-				<h2>${(data.user.name)}</h2>
-				<span>@${escape(data.user.handle)}</span>
-			</header>
-			<section class="tweet-section">
-				<p>${escape(data.content.text)}</p>
-			</section>
-			<footer class="tweet-footer">
-        <p>${new Date(data.created_at).toLocaleString()}<i class="fas fa-heart"></i><i class="fas fa-retweet"></i><i class="fas fa-flag"></i></p>
-			</footer>
-		</article>`
-}
+  function createTweetElement(data) {
+	  return `
+      <article class="tweet">
+        <header class="tweet-header">
+          <img class="avatar" src="${data.user.avatars.small}" />
+          <h2>${(data.user.name)}</h2>
+          <span>@${escape(data.user.handle)}</span>
+        </header>
+        <section class="tweet-section">
+          <p>${escape(data.content.text)}</p>
+        </section>
+        <footer class="tweet-footer">
+          <p>${new Date(data.created_at).toLocaleString()}<i class="fas fa-heart"></i><i class="fas fa-retweet"></i><i class="fas fa-flag"></i></p>
+        </footer>
+      </article>`
+  }
 
-// pass each tweet from array as object
-function renderTweets(newData) {
-	for (let tweet of newData) {
-		$('#tweet-container').prepend(createTweetElement(tweet));
-	}
-}
-
-$( "#submit" ).submit(function(event) {
-  event.preventDefault();
-    var newTweet = $(this).serialize();
-    if ($('textarea').val() === "" || $('textarea').val() === null) {
-      $('.error').text('You didn\'t fill out your tweet.').slideToggle('fast');
-    } else if ($('.counter').text() < 0) {
-      $('.error').text('Only 140 character tweets are allowed. Keep your tweets short and to the point').slideToggle('fast');
-    } else {
-      $.post('/tweets/', newTweet, function (data) {
-        $('#tweet-container').prepend(createTweetElement(data));
-        $('#submit')[0].reset();
-        $('.counter').text(140);
-      });
+  // pass each tweet from array as object
+  function renderTweets(newData) {
+    for (let tweet of newData) {
+      $('#tweet-container').prepend(createTweetElement(tweet));
     }
-});
+  }
 
-function loadTweets() {
-  $.get('/tweets/').then(function (newData) {
-    renderTweets(newData);
-  })
-}
+  $( "#submit" ).submit(function(event) {
+    event.preventDefault();
+      var newTweet = $(this).serialize();
+      if ($('textarea').val() === "" || $('textarea').val() === null) {
+        $('.error').text('You didn\'t fill out your tweet.').slideToggle('fast');
+      } else if ($('.counter').text() < 0) {
+        $('.error').text('Only 140 character tweets are allowed. Keep your tweets short and to the point').slideToggle('fast');
+      } else {
+        $.post('/tweets/', newTweet, function (data) {
+          $('#tweet-container').prepend(createTweetElement(data));
+          $('#submit')[0].reset();
+          $('.counter').text(140);
+        });
+      }
+  });
 
-loadTweets();
+  function loadTweets() {
+    $.get('/tweets/').then(function (newData) {
+      renderTweets(newData);
+    })
+  }
 
-$('.button').click(function () {
-  $('.new-tweet').slideToggle('fast');
-  $('#newTweetTextArea').focus();
-});
- 
+  loadTweets();
+
+  $('.button').click(function () {
+    $('.new-tweet').slideToggle('fast');
+    $('#newTweetTextArea').focus();
+  });
 });
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,19 +1,18 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
 
-// Defines helper functions for saving and getting tweets, using the database `db`
+// Defines helper functions for saving and getting tweets, using the mongo database `db`
 module.exports = function makeDataHelpers(db) {
   return {
 
-    // Saves a tweet to `db`
+    // Saves tweets to `db` now "mongodb"
     saveTweet: function(newTweet, callback) {
       db.collection("tweets").insertOne(newTweet);
       callback(null, true);
       
     },
 
-    // Get all tweets in `db`, sorted by newest first
+    // Get all tweets in `mongodb`, sorted by newest first
     getTweets: function(callback) {
       db.collection("tweets").find().toArray((err, tweets) => {
       if (err) {
